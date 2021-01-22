f8caa76d4a3886083a52b86dedfe05257aa39f8f
rjvatt <rjvatt@yandex.ru>
2021-01-22 09:06:23 +0300

    nodejs

---
+++ 22-01-2021/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.send("Hello world");
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
